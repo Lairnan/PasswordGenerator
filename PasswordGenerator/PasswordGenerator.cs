2bf182a5f52fc0cdf1e3b6e495fd38db6bdb909f
Alexey Saulin <alexey.saulin03@mail.ru>
2022-04-25 20:06:23 +0300

    Commit full project

---
+++ PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication2;
+
+public class PasswordGenerator
+{
+    private static readonly Random Random = new Random();
+    public string Password(int length)
+    {
+        var password = "";
+        var pass = new List<string>
+        {
+            "ABCDEFGHIJKLMNOPQRSTUXYZ",
+            "abcdefghijklmnopqrstuxyz",
+            "0123456789",
+            "!@#$%^&*()_+-:;\"'\\|/,.<>[]{}~"
+        };
+        for (int i = 0; i < length; i++)
+        {
+            var k = Random.Next(0, pass.Count);
+            password += pass[k][Random.Next(0, pass[k].Length)].ToString();
+        }
+
+        return password;
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal static class Program
+    {
+        private static readonly Random Random = new Random();
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Введите количество паролей: ");
+                var count = int.Parse(Console.ReadLine() ?? string.Empty);
+                var password = new PasswordGenerator();
+                for (var i = 0; i < count; i++)
+                {
+                    var length = Random.Next(5, 25);
+                    Console.WriteLine(password.Password(length));
+                }
+
+                Console.WriteLine("Вы вышли из программы.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
